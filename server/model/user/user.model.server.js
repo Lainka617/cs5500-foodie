d4e5c641c48003b05e63a247f3b4f90ab73b3ec1
Lainka617 <luo.shiq@husky.neu.edu>
2020-10-28 15:16:28 -0700

    add backend

---
+++ server/model/user/user.model.server.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const userSchema = require('./user.schema.server');
+//var bcrypt = require("bcrypt-nodejs");
+const userModel = mongoose.model('userModel', userSchema);
+
+// userService at server side will call this function
+userModel.createUser = createUser;
+userModel.findUserById = findUserById;
+userModel.findUserByUsername = findUserByUsername;
+userModel.findUserByCredentials = findUserByCredentials;
+userModel.updateUser = updateUser;
+userModel.deleteUser = deleteUser;
+userModel.findAllUsersByType = findAllUsersByType;
+
+
+module.exports = userModel;
+
+// functions go here
+
+function findAllUsersByType(type) {
+    return userModel.find({ userType: type});
+
+}
+function createUser(user) {
+    return userModel.create(user);
+}
+
+function findUserById(userId) {
+    return userModel.findById(userId);
+}
+
+function findUserByUsername(username) {
+    return userModel.findOne({ username: username });
+}
+
+function findUserByCredentials(username, password) {
+    return userModel.findOne({ username: username, password: password });
+}
+
+function updateUser(userId, user) {
+    return userModel.findByIdAndUpdate(userId, user, {new: true});
+}
+
+function deleteUser(userId) {
+    return userModel.findByIdAndRemove(userId);
+}
+
+
