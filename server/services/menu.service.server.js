d376782ee6a9e53fc3121984b9567d8b8dc45dd0
ToriLiu <toriliu1991@gmail.com>
2020-12-01 15:58:11 -0800

    updated restaurant/menu service

--- server/services/menu.service.server.js
+++ server/services/menu.service.server.js
@@ -3,15 +3,13 @@ module.exports = function (app) {
     var menuModel = require("../model/menu/menu.model.server.js");
     var path = require('path');
     const multer = require('multer'); // npm install multer --save
-    const baseUrl = '';
-    //const baseUrl = "http://localhost:8080"
+    const baseUrl = "http://localhost:3200"
 
-    app.post("/api/restaurant/menu", createDish);
-    app.get("/api/menu", findAllDishesForRestaurant);
+    app.post("/api/restaurant/menu/:restaurantId", createDish);
+    app.get("/api/restaurant/getmenu/:restaurantId", findAllDishesForRestaurant);
     app.get("/api/restaurant/menu/:did", findDishById);
     app.put("/api/restaurant/menu/:did", updateDish);
     app.delete("/api/restaurant/menu/:did", deleteDish);
-    app.put("/api/restaurant/dish?", reorderDishes);
 
     var storage = multer.diskStorage({
         destination: __dirname + '/../../dist/web5610/assets/uploads/',
@@ -23,12 +21,61 @@ module.exports = function (app) {
         storage: storage
     }).single("myFile");
     //UPLOAD
-//    app.post ("/api/upload", upload, uploadImage);
-//    app.get("/api/image/:imageName", findImage);
+    app.post ("/api/upload", upload, uploadImage);
+    app.get("/api/image/:imageName", findImage);
+
+    function findImage(req, res) {
+        var imageName = req.params.imageName;
+        res.sendFile(path.resolve("/../../src/assets/uploads/" + imageName));
+    }
+
+    function uploadImage(req, res) {
+        var dishId = req.body.dishId;
+        var width = req.body.width;
+        var myFile = req.file;
+        var name = req.body.name;
+
+        // condition when myFile is null
+        const callbackUrl = baseUrl + '/#/restaurant/menu/'+ dishId;
+        if (myFile == null) {
+            res.redirect(callbackUrl);
+            return;
+        }
+
+        var originalname = myFile.originalname; // file name on user's computer
+        var filename = myFile.filename; // new file name in upload folder
+        //var filepath = myFile.path; // full path of uploaded file
+        var destination = myFile.destination; // folder where file is saved to
+        var size = myFile.size;
+        var mimetype = myFile.mimetype;
+
+        if (dishId === 'new') {
+            var dish = {_id: 'new', dish_name: '', price: 0, description: '', url: ''};
+            dish._id = (new Date()).getTime().toString();
+            dish.url = baseUrl + '/assets/uploads/' + filename;
+
+            console.log('create dish image: ' + dish._id);
+            dishs.push(dish);
+            res.redirect(callbackUrl);
+            return;
+        }
+
+        var imageUrl = baseUrl + "/assets/uploads/" + filename;
+        var dish = { url: imageUrl };
+        menuModel
+            .updateDish(dishId, dish)
+            .then(function (stats) {
+                    res.send(200);
+                },
+                function (err) {
+                    res.sendStatus(404).send(err);
+                });
+        res.redirect(callbackUrl);
+    }
 
     function createDish(req, res) {
         // var pageId = req.params.pageId;
-        var dish = _.pick(req.body, ["dish_name",  "description", "url", "price"]);
+        var dish = _.pick(req.body, ["dish_name",  "description", "url", "price",  "restaurantId"]);
         console.log(dish);
         console.log(req.body);
         menuModel.createDish(dish).then(
@@ -46,8 +93,8 @@ module.exports = function (app) {
     }
 
     function findAllDishesForRestaurant(req, res) {
-
-        menuModel.findAllDishes().then(
+        var restaurantId = req.params['restaurantId'];
+        menuModel.findAllDishes(restaurantId).then(
             function (dish) {
                 res.json(dish);
                 console.log(dish);
