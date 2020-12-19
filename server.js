63a5fa00e9848258d2d8c8a6bf884430b1fd61bd
ToriLiu <toriliu1991@gmail.com>
2020-12-19 09:39:48 -0800

    config for deploy

--- server.js
+++ server.js
@@ -23,16 +23,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://cs5500-foodie.herokuapp.com");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://cs5500-foodie.herokuapp.com");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
 
-var cors = require('cors');    
-app.use(cors({credentials: true, origin: 'https://cs5500-foodie.herokuapp.com'}));
+// var cors = require('cors');    
+// app.use(cors({credentials: true, origin: 'https://cs5500-foodie.herokuapp.com'}));
 
 const port=process.env.PORT || '80';
 app.set('port', port);
@@ -52,10 +52,13 @@ require("./server/app")(app);
 // Point static path to dist -- For building -- REMOVE
 app.use(express.static(path.join(__dirname, 'dist/cs5500')));
 app.use(express.static(path.join(__dirname, 'public/')));
-app.get('/**', function (req, res) {
-  res.sendFile(__dirname + '/dist/cs5500/index.html');
-});
+// app.get('/**', function (req, res) {
+//   res.sendFile(__dirname + '/dist/cs5500/index.html');
+// });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 server.listen( port , function() {
   console.log('Node app is running on port', app.get('port'))});
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://cs5500-FoodieOverflow.herokuapp.com'
+  baseUrl: 'https://cs5500-foodie.herokuapp.com'
 };
