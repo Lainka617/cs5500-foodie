1095e4c71c01c02196eff04e1f42665c13e421a3
Lainka617 <luo.shiq@husky.neu.edu>
2020-12-19 11:36:41 -0800

    update2

--- server.js
+++ server.js
@@ -25,28 +25,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://cs5500-foodie.herokuapp.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
-var cors = require('cors');    
-app.use(cors({credentials: true, origin: 'https://cs5500-foodie.herokuapp.com'}));
+//var cors = require('cors');    
+//app.use(cors({credentials: true, origin: 'https://cs5500-foodie.herokuapp.com'}));
 
 const port=process.env.PORT || '80';
 app.set('port', port);
 
 // Create HTTP server
 const server = http.createServer(app);
-
-// For Build: Catch all other routes and return the index file -- BUILDING
-//app.get('*', function (req, res) {
-//  res.sendFile(path.join(__dirname, 'dist/index.html'));
-//});
-
-
 
 require("./server/app")(app);
 
